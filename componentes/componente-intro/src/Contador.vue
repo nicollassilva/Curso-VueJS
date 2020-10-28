d5a8ef39af89608e5c325747faf9b601a0721520
Nícollas Silva <lyod.hp@gmail.com>
2020-10-28 17:28:48 -0300

    Style Scoped

--- componentes/componente-intro/src/Contador.vue
+++ componentes/componente-intro/src/Contador.vue
@@ -24,14 +24,14 @@ export default {
 }
 </script>
 
-<style>
-    .contador span {
+<style scoped>
+    span {
         border-bottom: 1px solid #ccc;
         height: 30px;
         padding: 5px 25px;
     }
 
-    .contador button {
+    button {
         border-radius: 15px;
         height: 30px;
         width: 30px;
