6ebb90fb332c4a796a429d9ec1bf36656b15c2c5
Nícollas Silva <lyod.hp@gmail.com>
2020-11-04 11:32:13 -0300

    Props de Componentes

---
+++ componentes/componente-comunicacao/src/components/UsuarioInfo.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Vários detalhes...</p>
+        <p>Nome do Usuário: <strong>{{ inverterNome() }}</strong></p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        //nome: String [String, Array]
+        nome: {
+            type: String,
+            required: true,
+            default: 'Anônimo'
+        }
+    },
+    data() {
+        return {
+            nomeAlternativo: this.nome
+        }
+    },
+    methods: {
+        inverterNome() {
+            return this.nome.split('').reverse().join('')
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+</style>
